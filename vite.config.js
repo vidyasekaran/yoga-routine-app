9c196d4f28427d9b320f3435407a53d66d742edd
Vidyasekaran Duvarakan <vidguru1008@gmail.com>
2025-11-27 15:37:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/yoga-routine-app/", // ← exact name of your repo
+  base: '/yoga-routine-app/'
 });
